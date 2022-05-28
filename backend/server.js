a3cac21c8a7e4ba1fa330da082144b4a54274759
mathieuelisa <mathieu.elisa@gmail.com>
2022-05-28 16:09:34 +0200

    Adding test page and config the server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log("server...");
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "ceci est un test" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
